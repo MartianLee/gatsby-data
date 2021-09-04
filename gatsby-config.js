823b8f1afc89f6f21893fb02d06a2ba0e1242074
Kahlo <martionlee@gmail.com>
2021-09-04 13:30:11 +0900

    컨텐츠를 추가하고 mdx 포맷과 rechart를 적용합니다

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,61 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-transformer-remark",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `notes`,
+        path: `${__dirname}/src/notes/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/projects`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        // gatsbyRemarkPlugins: [
+        //   `gatsby-remark-prismjs`,
+        //   {
+        //     resolve: `gatsby-remark-images`,
+        //     options: {
+        //       maxWidth: 1000,
+        //       linkImagesToOriginal: false,
+        //       withWebp: true,
+        //     },
+        //   },
+        // ],
+      },
+    },
+    `gatsby-plugin-emotion`,
+  ],
+  siteMetadata: {
+    title: "SEONGHWA BLOG",
+    description: "web dev porfolio",
+    copyright: "This is copyright 2021 seonghwa",
+    contact: "me@seonghwakr.com",
+  },
 }
---
+++ gatsby-node.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+exports.createPages = async ({ actions, graphql }) => {
+  // const { data } = await graphql(`
+  //   query Projects {
+  //     allMarkdownRemark {
+  //       nodes {
+  //         frontmatter {
+  //           slug
+  //         }
+  //       }
+  //     }
+  //   }
+  // `)
+  const { data } = await graphql(`
+    query Projects {
+      allMdx(sort: { fields: frontmatter___date, order: DESC }) {
+        nodes {
+          frontmatter {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.allMdx.nodes.forEach(node => {
+    actions.createPage({
+      path: `/projects/${node.frontmatter.slug}`,
+      component: path.resolve("./src/templates/project-details.js"),
+      context: { slug: node.frontmatter.slug },
+    })
+  })
+  // actions.createPage({
+  //   path: `/projects/graphql-test`,
+  //   component: path.resolve("./src/templates/project-details.js"),
+  //   context: { slug: 'graphql-test' },
+  // })
+}
